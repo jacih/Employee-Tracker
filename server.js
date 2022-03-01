e62458c2af11ebfafc206d5b1c0b1b3e17c372a6
Jacqueline Hodgson <jacqueline.e.hodgson@gmail.com>
2022-03-01 09:21:21 -0500

    shell file commits

---
+++ server.js
@@ -0,0 +1,6 @@
+const inquirer = require('inquirer');
+const mysql = require('mysql');
+const Department = require(__dirname + '/classes/Department.js');
+const Role = require(__dirname + '/classes/Role.js');
+const Employee = require(__dirname + '/classes/Employee.js');
+
